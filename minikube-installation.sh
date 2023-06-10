dd41615e677a990ea49c3f7bb83fa94d631fff68
Farasat Khan <farasatkahan@gmail.com>
2023-06-10 16:31:09 +0500

    Update minikube-installation.sh

--- minikube-installation.sh
+++ minikube-installation.sh
@@ -6,13 +6,16 @@ apt install curl
 # Step 2: Install Docker
 apt install -y docker.io
 
-# Step 3: Download kubectl binary
+# Step 3: Add current user to the docker group
+usermod -aG docker $USER && newgrp docker
+
+# Step 4: Download kubectl binary
 curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
 
-# Step 4: Download kubectl SHA256 checksum
+# Step 5: Download kubectl SHA256 checksum
 curl -LO https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256
 
-# Step 5: Verify kubectl SHA256 checksum
+# Step 6: Verify kubectl SHA256 checksum
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 
 if [ $? -ne 0 ]; then
@@ -20,20 +23,17 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Step 6: Install kubectl
+# Step 7: Install kubectl
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-# Step 7: Check kubectl version
+# Step 8: Check kubectl version
 kubectl version --client
 
-# Step 8: Download minikube binary
+# Step 9: Download minikube binary
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 
-# Step 9: Install minikube
+# Step 10: Install minikube
 install minikube-linux-amd64 /usr/local/bin/minikube
-
-# Step 10: Add current user to the docker group
-usermod -aG docker $USER && newgrp docker
 
 # Step 11: Start minikube with Docker driver
 minikube start --driver=docker
